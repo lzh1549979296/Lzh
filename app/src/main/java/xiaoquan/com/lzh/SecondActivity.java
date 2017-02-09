328baa3acf3d9a03bd52e7511a0b5f9a10e92b31
Liu ZhiHua <liuzhihua@xiaoquan.tv>
2017-02-09 20:11:56 +0800

    第一次提交

---
+++ app/src/main/java/xiaoquan/com/lzh/SecondActivity.java
@@ -0,0 +1,16 @@
+package xiaoquan.com.lzh;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LZH on 2017/2/9.
+ */
+
+public class SecondActivity  extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
