48de0596f491d2783fa5db764ab8db503e995d76
Liu ZhiHua <liuzhihua@xiaoquan.tv>
2017-02-09 20:16:20 +0800

    第二次提交

--- app/src/main/java/xiaoquan/com/lzh/MainActivity.java
+++ app/src/main/java/xiaoquan/com/lzh/MainActivity.java
@@ -2,12 +2,21 @@ package xiaoquan.com.lzh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+            ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //这是新增的项目
+        Log.i(TAG, "onCreate: "+"hahah");
+
+
     }
 }
--- app/src/main/java/xiaoquan/com/lzh/SecondActivity.java
+++ app/src/main/java/xiaoquan/com/lzh/SecondActivity.java
@@ -3,14 +3,20 @@ package xiaoquan.com.lzh;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by LZH on 2017/2/9.
  */
 
 public class SecondActivity  extends AppCompatActivity{
+    private static final String TAG = "SecondActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        Log.i(TAG, "onCreate: "+"第二次提交");
     }
 }
